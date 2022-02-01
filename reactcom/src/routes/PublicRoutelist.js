a40743616b07c69fe2a8b8733c737b0ef517801c
Enser Uğurcan Ateşçakmak <enser3678@hotmail.com>
2022-02-01 21:10:48 +0300

    react

---
+++ reactcom/src/routes/PublicRoutelist.js
@@ -0,0 +1,80 @@
+import About from "../components/frontend/About";
+import Contact from "../components/frontend/Contact";
+import Page404 from "../components/errors/Page404";
+import Page403 from "../components/errors/Page403";
+import Login from "../components/frontend/auth/Login";
+import Register from "../components/frontend/auth/Register";
+import forgotPassword from "../components/frontend/auth/ForgotPassword";
+import resetPassword from "../components/frontend/auth/resetPassword";
+import ViewCategory from "../components/frontend/collections/ViewCategory";
+import ViewProduct from "../components/frontend/collections/ViewProduct";
+import Content from "../layouts/frontend/Content";
+import ProductDetail from "../components/frontend/collections/ProductDetail";
+import Cart from "../components/frontend/Cart";
+import Wishlist from "../components/frontend/Wishlist";
+import Checkout from "../components/frontend/Checkout";
+import Profile from "../components/frontend/Profile";
+import Myorders from "../components/frontend/Myorders";
+import Help from "../components/frontend/Help";
+const PublicRoutelist = [
+  { path: "/", exact: true, name: "Home", component: Content },
+  { path: "/about", exact: true, name: "About", component: About },
+  { path: "/contact", exact: true, name: "Contact", component: Contact },
+  { path: "/403", exact: true, name: "Page403", component: Page403 },
+  { path: "/404", exact: true, name: "page404", component: Page404 },
+  { path: "/login", exact: true, name: "Login", component: Login },
+  {
+    path: "/forgotPassword",
+    exact: true,
+    name: "forgotPassword",
+    component: forgotPassword,
+  },
+  {
+    path: "/resetPassword",
+    exact: true,
+    name: "resetPassword",
+    component: resetPassword,
+  },
+  { path: "/register", exact: true, name: "Register", component: Register },
+  { path: "/orders", exact: true, name: "Myorders", component: Myorders },
+  { path: "/help", exact: true, name: "Help", component: Help },
+
+  {
+    path: "/collections",
+    exact: true,
+    name: "ViewCategory",
+    component: ViewCategory,
+  },
+  {
+    path: "/collections/:slug",
+    exact: true,
+    name: "ViewProduct",
+    component: ViewProduct,
+  },
+  {
+    path: "/collections/:category/:product",
+    exact: true,
+    name: "ProductDetail",
+    component: ProductDetail,
+  },
+  {
+    path: "/cart",
+    exact: true,
+    name: "Cart",
+    component: Cart,
+  },
+  {
+    path: "/wishlist",
+    exact: true,
+    name: "Wishlist",
+    component: Wishlist,
+  },
+  {
+    path: "/checkout",
+    exact: true,
+    name: "Checkout",
+    component: Checkout,
+  },
+  { path: "/profile", exact: true, name: "Profile", component: Profile },
+];
+export default PublicRoutelist;
